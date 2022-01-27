306a700407bec628b3445ca483f633d8aac885e6
//Sol!tude <evan3bal@gmail.com>
2022-01-27 15:49:03 +0200

    Initial commit

---
+++ statrpc.py
@@ -0,0 +1,16 @@
+import psutil; import time
+from pypresence import Presence
+from gpuinfo import GPUInfo
+
+client_id = '818263820756713522'  # Fake ID, put your real one here
+RPC = Presence(client_id,pipe=0)  # Initialize the client class
+RPC.connect() # Start the handshake loop
+
+
+while True:  # The presence will stay on as long as the program is running
+    cpu_per = int(psutil.cpu_percent()) # Get CPU Usage
+    gpu_per = GPUInfo.gpu_usage()[0][0]
+    mem = psutil.virtual_memory()
+    mem_per = int(psutil.virtual_memory().percent)
+    print(RPC.update(details="RAM: " + str(mem_per) + "%,  " + "CPU: " + str(cpu_per) + "%,  " + "GPU: " + str(gpu_per) + "%" , state="Why did I even make this?"))  # Set the presence
+    time.sleep(3) # Can only update rich presence every 15 seconds
